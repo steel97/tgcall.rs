9011b79bd127d88c860264bc58fb7fd2f7db5807
Ivan Yv <steel-97@mail.ru>
2024-08-21 04:31:00 +0300

    Added prebuilt tdlib

---
+++ run.bat
@@ -0,0 +1,2 @@
+set LOCAL_TDLIB_PATH=%cd%/tdlibs/windows/x86_64/tdlib
+cargo run
---
+++ tdlibs/windows/x86_64/tdlib/lib/cmake/Td/TdStaticTargets-release.cmake
@@ -0,0 +1,109 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Td::tdutils" for configuration "Release"
+set_property(TARGET Td::tdutils APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdutils PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdutils.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdutils )
+list(APPEND _cmake_import_check_files_for_Td::tdutils "${_IMPORT_PREFIX}/lib/tdutils.lib" )
+
+# Import target "Td::tdactor" for configuration "Release"
+set_property(TARGET Td::tdactor APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdactor PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdactor.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdactor )
+list(APPEND _cmake_import_check_files_for_Td::tdactor "${_IMPORT_PREFIX}/lib/tdactor.lib" )
+
+# Import target "Td::tdnet" for configuration "Release"
+set_property(TARGET Td::tdnet APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdnet PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdnet.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdnet )
+list(APPEND _cmake_import_check_files_for_Td::tdnet "${_IMPORT_PREFIX}/lib/tdnet.lib" )
+
+# Import target "Td::tdsqlite" for configuration "Release"
+set_property(TARGET Td::tdsqlite APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdsqlite PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdsqlite.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdsqlite )
+list(APPEND _cmake_import_check_files_for_Td::tdsqlite "${_IMPORT_PREFIX}/lib/tdsqlite.lib" )
+
+# Import target "Td::tddb" for configuration "Release"
+set_property(TARGET Td::tddb APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tddb PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tddb.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tddb )
+list(APPEND _cmake_import_check_files_for_Td::tddb "${_IMPORT_PREFIX}/lib/tddb.lib" )
+
+# Import target "Td::tdjson_static" for configuration "Release"
+set_property(TARGET Td::tdjson_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdjson_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdjson_static.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdjson_static )
+list(APPEND _cmake_import_check_files_for_Td::tdjson_static "${_IMPORT_PREFIX}/lib/tdjson_static.lib" )
+
+# Import target "Td::tdjson_private" for configuration "Release"
+set_property(TARGET Td::tdjson_private APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdjson_private PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdjson_private.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdjson_private )
+list(APPEND _cmake_import_check_files_for_Td::tdjson_private "${_IMPORT_PREFIX}/lib/tdjson_private.lib" )
+
+# Import target "Td::tdcore" for configuration "Release"
+set_property(TARGET Td::tdcore APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdcore PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdcore.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdcore )
+list(APPEND _cmake_import_check_files_for_Td::tdcore "${_IMPORT_PREFIX}/lib/tdcore.lib" )
+
+# Import target "Td::tdclient" for configuration "Release"
+set_property(TARGET Td::tdclient APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdclient PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdclient.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdclient )
+list(APPEND _cmake_import_check_files_for_Td::tdclient "${_IMPORT_PREFIX}/lib/tdclient.lib" )
+
+# Import target "Td::tdapi" for configuration "Release"
+set_property(TARGET Td::tdapi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Td::tdapi PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/tdapi.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Td::tdapi )
+list(APPEND _cmake_import_check_files_for_Td::tdapi "${_IMPORT_PREFIX}/lib/tdapi.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
